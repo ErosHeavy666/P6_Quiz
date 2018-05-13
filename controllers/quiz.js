420ebf66ccfefe324aa8d34983b1c99d426611a6
Eros García Arroyo <eros.garcia.arroyo@alumnos.upm.es>
2018-05-13 23:17:08 +0200

    septimo intento

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -197,11 +197,11 @@ exports.randomplay = (req, res, next) => {
 
 
 exports.randomcheck = (req, res, next) => {
-
+	/*
     const answer = req.query.answer || '';
     const result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
-     const score = req.session.resolved.length;
-     
+    const score = req.session.resolved.length;
+
     if (result) {
         if (req.session.resolved.indexOf(req.quiz.id) === -1){
             req.session.resolved.push(req.quiz.id);
@@ -221,5 +221,30 @@ exports.randomcheck = (req, res, next) => {
         delete req.session.resolved;
         res.render('quizzes/random_result', {result, score, answer});
     }
+	*/
+	 try{
+    const {quiz, query} = req;
+    req.session.alreadyPlayed = req.session.alreadyPlayed || [];
 
+    const actual_answer = query.answer || "";
+    const right_answer = quiz.answer;
+
+    const result = actual_answer.toLowerCase().trim() === right_answer.toLowerCase().trim();
+
+    if (result){
+        if(req.session.alreadyPlayed.indexOf(req.quiz.id) === -1){ 
+            req.session.alreadyPlayed.push(req.quiz.id);
+        }
+    } 
+
+    const score = req.session.alreadyPlayed.length;
+
+    if(!result){
+        req.session.alreadyPlayed = [];
+    }
+
+    res.render('quizzes/random_result', {actual_answer, result, score});
+    } catch (error){
+        next(error);
+    }
 };
